5c39bf45c499ad55e0c7ca6cf167293d2946fe0d
coyarzun <imavoodoochild@gmail.com>
2016-08-06 12:59:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+//20160806 @fcaMedios
+//laerning p5.js
+
+function setup() {
+  //createCanvas(400,400);
+  createCanvas(windowWidth,windowHeight);
+  background(255,0,0);
+}
+
+function draw() {
+  ellipse(mouseX,mouseY,10,10)
+}
